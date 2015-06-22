0f64d061d7ec75c026d5ec4d7f3c93f3b9479f8d
goofolph <cbransonc14@gmail.com>
2015-06-21 21:17:33 -0400

    Added basic movement

--- ComputerCraft/API/goofTurtle.lua
+++ ComputerCraft/API/goofTurtle.lua
@@ -1,2 +1,42 @@
 -- chopTree.lua
 -- by Goofolph https://github.com/goofolph
+
+-- tables for easy function lookups
+digTable = {
+  "forward" = turtle.dig,
+  "down" = turtle.digDown,
+  "up" = turtle.digUp
+}
+moveTable = {
+  "forward" = turtle.forward,
+  "down" = turtle.down,
+  "up" = turtle.up
+}
+turnTable = {
+  "right" = turtle.turnRight,
+  "left" = turtle.turnLeft
+}
+oppositeDir = {
+  "left" = "right",
+  "right" = "left",
+  "up" = "down",
+  "down" = "up",
+  "forward" = "back",
+  "back" = "forward"
+}
+
+function turn (dir, count)
+  i = 0
+  while i < count do
+    turnTable[dir] ()
+    i = i + 1
+  end
+end
+
+function digMove (dir, distance, grav)
+  if dir == "left" or dir == "right" then
+    turn (dir, 1)
+  elseif dir == "back" then
+    turn ("left", 2)
+  end
+end
--- ComputerCraft/API/goofTurtle.md
+++ ComputerCraft/API/goofTurtle.md
@@ -4,4 +4,4 @@ API for common functions used in many of my turtle scripts.
 
 to load add this line at the beginning of any program
 
-os.loadAPI("goofTurtle.lua")
+`os.loadAPI("goofTurtle.lua")`
