3274d7eb9a9e98f76bada304f6ba07664ccbd6ab
goofolph <cbransonc14@gmail.com>
2015-06-25 20:49:04 -0400

    added TODOs and sapling refilling

--- ComputerCraft/Turtles/chopTree.lua
+++ ComputerCraft/Turtles/chopTree.lua
@@ -3,9 +3,12 @@
 
 local args = {...}
 
+goofTurtle = os.loadAPI("goofTurtle.lua")
+
 local saplingSlot = 15
 local bonemealSlot = 16
 
+-- don't change these dirs (no support for changes yet)
 local outputDir = "down"
 local saplingDir = "right"
 local bonemealDir = "left"
@@ -14,6 +17,12 @@ local numSaplings = 4
 local numBonemeal = 64
 
 -- TODO refill inventory
+if turtle.getItemCount(saplingSlot) < numSaplings then
+  goofTurtle.turn(saplingDir, 1)
+  turtle.select(saplingSlot)
+  turtle.suck(numSaplings - turtle.getItemCount(saplingSlot))
+  goofTurtle.turn(goofTurtle.oppositeDir(saplingDir))
+end
 
 -- TODO chop down the tree
 
