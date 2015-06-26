c5ab41b8289fa8e6812433333afa487bce30e4b7
goofolph <cbransonc14@gmail.com>
2015-06-25 20:05:12 -0400

    added error checking. changed to fs api because io is different in computercraft

--- ComputerCraft/down.lua
+++ ComputerCraft/down.lua
@@ -4,6 +4,7 @@ local args = {...}
 
 -- check for url
 if not args[1] then
+  print("No url given")
   return false
 end
 
@@ -11,16 +12,24 @@ local url = args[1]
 
 -- check for filename
 if not args[2] then
+  print("No filename given")
   return false
 end
 local filename = args[2]
 
 download = http.get(url)
 if download then
-  file = io.open(filename);
-  file:write(download.readAll())
-  file:close()
-  return true
+  file = fs.open(filename);
+  if file then
+    file:write(download.readAll())
+    file:close()
+    print("download successful")
+    return true
+  else
+    print("error opening file")
+    return false
+  end
 else
+  print("error downloading file")
   return false
 end
