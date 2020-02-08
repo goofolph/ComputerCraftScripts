d2b6ee2971618e27c32e9999e8243c96b27007ab
Caleb Branson <cbransonc14@gmail.com>
2020-02-08 05:52:18 -0500

    refuel and dig actions

--- Turtles/TurtleActions.lua
+++ Turtles/TurtleActions.lua
@@ -11,7 +11,10 @@
 
 -- delay between mining a block or attacking an entity before attempting to move
 -- again. eg (sand/gravel falling, mob walking forward)
-moveDelay = 0.5
+local moveDelay = 0.5
+
+-- minFuel to have at all times
+local minFuel = 10
 
 -- Moves turtle relative from current position.
 --
@@ -46,7 +49,6 @@ end
 --      status is true if there were no errors, false otherwise. dist is there
 --          in case taraget distance could not be reached
 function forward(dist, mine, attack)
-    print('Moving ' .. dist .. ' forwards')
     dist = dist or 1
     for i = 1,dist,1 do
         while not turtle.forward() do
@@ -75,7 +77,6 @@ end
 --      status is true if there were no errors, false otherwise. dist is there
 --          in case taraget distance could not be reached
 function up(dist, mine, attack)
-    print('Moving ' .. dist .. ' up')
     dist = dist or 1
     for i = 1,dist,1 do
         while not turtle.up() do
@@ -104,7 +105,6 @@ end
 --      status is true if there were no errors, false otherwise. dist is there
 --          in case taraget distance could not be reached
 function down(dist, mine, attack)
-    print('Moving ' .. dist .. ' down')
     dist = dist or 1
     for i = 1,dist,1 do
         while not turtle.down() do
@@ -134,7 +134,6 @@ end
 --      status is true if there were no errors, false otherwise. dist is there
 --          in case taraget distance could not be reached
 function back(dist, mine, attack, keepFace)
-    print('Moving ' .. dist .. ' back')
     dist = dist or 1
     turtle.turnLeft()
     turtle.turnLeft()
@@ -159,7 +158,6 @@ end
 --      status is true if there were no errors, false otherwise. dist is there
 --          in case taraget distance could not be reached
 function right(dist, mine, attack, keepFace)
-    print('Moving ' .. dist .. ' right')
     dist = dist or 1
     turtle.turnRight()
     local retStatus, retDist = forward(dist, mine, attack)
@@ -182,7 +180,6 @@ end
 --      status is true if there were no errors, false otherwise. dist is there
 --          in case taraget distance could not be reached
 function left(dist, mine, attack, keepFace)
-    print('Moving ' .. dist .. ' left')
     dist = dist or 1
     turtle.turnLeft()
     local retStatus, retDist = forward(dist, mine, attack)
@@ -192,3 +189,122 @@ function left(dist, mine, attack, keepFace)
     return retStatus, retDist
 end
 
+-- Counts number of free inventory slots.
+--
+-- Returns:
+--      number of free inventory slots
+function freeSlots()
+    local free = 16
+    for i = 1,16,1 do
+        if turtle.getItemCount(i) > 0 then
+            free = free-1
+        end
+    end
+    return free
+end
+
+-- Ensures turtle has desired amount of fuel.
+--
+-- Args:
+--      targetFuel: amount of desired fuel
+--
+-- Returns:
+--      true on success, false otherwise
+function refuel(targetFuel)
+    targetFuel = math.max(targetFuel, minFuel)
+    if turtle.getFuelLevel() == "unlimited" or turtle.getFuelLevel() >= targetFuel then
+        return true
+    end
+    for i = 1,16,1 do
+        turtle.select(i)
+        while turtle.refuel(1) do
+            if turtle.getFuelLevel() >= targetFuel then
+                return true
+            end
+        end
+    end
+    return false
+end
+
+-- Digs block in front until clear.
+--
+-- Continues to dig until area is clear to handle falling sand/gravel/cobblegen.
+--
+-- Returns:
+--      true on success, false otherwise
+function dig()
+    while turtle.detect() do
+        if not turtle.dig() then
+            return false
+        else
+            sleep(moveDelay)
+        end
+    end
+    return true
+end
+
+-- Digs block in front until clear.
+--
+-- Continues to dig until area is clear to handle falling sand/gravel/cobblegen.
+--
+-- Returns:
+--      true on success, false otherwise
+function digUp()
+
+    while turtle.detectUp() do
+        if not turtle.digUp() then
+            return false
+        else
+            sleep(moveDelay)
+        end
+    end
+    return true
+end
+
+-- Digs block in front until clear.
+--
+-- Continues to dig until area is clear to handle falling sand/gravel/cobblegen.
+--
+-- Returns:
+--      true on success, false otherwise
+function digDown()
+
+    while turtle.detectDown() do
+        if not turtle.digDown() then
+            return false
+        else
+            sleep(moveDelay)
+        end
+    end
+    return true
+end
+
+-- Digs block in front until clear.
+--
+-- Continues to dig until area is clear to handle falling sand/gravel/cobblegen.
+--
+-- Returns:
+--      true on success, false otherwise
+function digRight(keepFace)
+    turtle.turnRight()
+    local ret = dig()
+    if keepFace then
+        turtle.turnLeft()
+    end
+    return ret
+end
+
+-- Digs block in front until clear.
+--
+-- Continues to dig until area is clear to handle falling sand/gravel/cobblegen.
+--
+-- Returns:
+--      true on success, false otherwise
+function digLeft(keepFace)
+    turtle.turnLeft()
+    local ret = dig()
+    if keepFace then
+        turtle.turnRight()
+    end
+    return ret
+end
